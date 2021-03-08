2e843599b509b7ce831506afb4801ec6e7aab696
Phu0903 <71036328+Phu0903@users.noreply.github.com>
2021-03-09 01:17:52 +0700

    lll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+// var p1 = {
+//   ten :"man hinh",
+//   gia:90
+// }
+// console.log(p1);
+// var p2 = {...p1} ; 
+// p2.gia =100;
+// console.log(p2);
+// console.log(p1);
+
+var redux = require('redux');
+var oldState = {
+  num:["man hinh","chuot","ban phim"],
+  editstatus:true
+}
+var reducer1 = (state=oldState,action) =>{
+  switch (action.type) {
+    case "CHANGE_EDIT_STATUS":
+       return {...state,editstatus:!state.editstatus}
+      break;
+      case "ADD_NEW":
+        return {...state,num:[...state.num,action.newItem]}
+       break;
+      case "DELETE":
+        return {...state,num:state.num.filter((value,i)  => i !== action.index
+          )}
+          break;
+    default:
+      return state
+      break;
+  }
+  
+}
+
+var store1 =redux.createStore(reducer1);
+store1.subscribe(()=>{ //hàm theo dõi 
+  console.log(JSON.stringify(store1.getState()));
+})
+store1.dispatch({type:"CHANGE_EDIT_STATUS"})
+
+store1.dispatch({
+  type:"ADD_NEW",
+  newItem:"Tai Nghe"
+})
+
+
+store1.dispatch({
+  type:"DELETE",
+  index:0
+})
 
 function App() {
   return (
